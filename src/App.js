f7da70fde27f6be467a20e430fba90153ca5fac5
oluseyi-ged <oluseyi.ged@gmail.com>
2021-08-05 11:44:34 +0100

    twitter feed header done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css"
 import Sidebar from "./Sidebar"
+import Feed from "./Feed"
+import Widget from "./Widget"
 
 function App() {
   return (
     <div className="app">
-      {/* Sidebar */}
       <Sidebar />
-
-      {/* Feed */}
-      {/* Widgets */}
+      <Feed />
+      <Widget />
     </div>
   )
 }
---
+++ src/Feed.js
@@ -0,0 +1,22 @@
+import React from "react"
+import "./Feed.css"
+
+function Feed() {
+  return (
+    <div className="feed">
+      <div className="feed__header">
+        <h2>Home</h2>
+      </div>
+      {/* tweet box */}
+
+      {/* post */}
+      {/* post */}
+      {/* post */}
+      {/* post */}
+      {/* post */}
+      {/* post */}
+    </div>
+  )
+}
+
+export default Feed
