15d9b4cfa05d8c6231a61401f8008ff9637ca8e4
oluseyi-ged <oluseyi.ged@gmail.com>
2021-08-05 11:08:56 +0100

    dummy twitter sidebar implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css"
+import Sidebar from "./Sidebar"
 
 function App() {
   return (
     <div className="app">
-      <h1>Twitter clone</h1>
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* Feed */}
+      {/* Widgets */}
     </div>
   )
 }
