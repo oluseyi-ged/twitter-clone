0fdd9b6028a559a649b3ca88ecb1de5f7c8ee806
oluseyi-ged <oluseyi.ged@gmail.com>
2021-08-05 10:07:34 +0100

    Twitter Clone react setup

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB3H8-F7pqEnuh3RmzP0allGnDX6Ue1WUc",
+  authDomain: "twitter-dummy.firebaseapp.com",
+  projectId: "twitter-dummy",
+  storageBucket: "twitter-dummy.appspot.com",
+  messagingSenderId: "375891987157",
+  appId: "1:375891987157:web:fba55762907815d0c6f05c",
+  measurementId: "G-29SZ0TTMY1",
+}
